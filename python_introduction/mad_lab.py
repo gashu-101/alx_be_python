64ddd57def46614b1f6213613c049fa34099a0c1
gashu-101 <165908298+gashu-101@users.noreply.github.com>
2024-06-27 20:27:32 +0300

    Add files via upload

---
+++ python_introduction/mad_lab.py
@@ -0,0 +1,3 @@
+user = input("Enter a word: ")
+story = f"On a beautiful {user} day, I went to the zoo. I saw a funny {user} monkey swinging from the trees. Then, I spotted a majestic {user} lion lounging in the sun.  What a wild and {user} experience!"
+print(story)
